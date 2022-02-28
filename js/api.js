2b32751480b37661ccbc680793e063fcb81e6502
Emarat <emarathossain00@gmail.com>
2022-03-01 03:08:41 +0600

    second commit

--- js/api.js
+++ js/api.js
@@ -1,1 +1,18 @@
-console.log('let connected');
+// console.log('let connected');
+// access inputField & button  using arrow function
+inputField = () => {
+    const userInput = document.getElementById('inputForm');
+    const searchText = userInput.value;
+    const Btn = document.getElementById('button-addon2');
+    // console.log(searchText);
+    userInput.value = '';
+    // fetch api 
+    const url = `
+    https://openapi.programming-hero.com/api/phones?search=${searchText}
+    `;
+    // console.log(url);
+    fetch(url)
+        .then(res => res.json())
+        .then(data => console.log(data))
+}
+
