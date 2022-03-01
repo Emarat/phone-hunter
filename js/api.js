0233e2228ff546a341c92fd8ba3a6a9acfdf670d
Emarat <emarathossain00@gmail.com>
2022-03-01 17:51:15 +0600

    error validation

--- js/api.js
+++ js/api.js
@@ -1,24 +1,37 @@
-// console.log('let connected');
+const error = document.getElementById('error');
 // access inputField & button  using arrow function
 const inputField = () => {
     const userInput = document.getElementById('inputForm');
     const searchText = userInput.value;
     const Btn = document.getElementById('button-addon2');
     // console.log(searchText);
+    // clear inputfield 
     userInput.value = '';
-    // fetch api 
-    const url = `
+    /* empty input field search error validation */
+    if (searchText == '') {
+        error.innerText = 'Please Write a Valid Keyword';
+    } else {
+        error.innerText = '';
+        // fetch api 
+        const url = `
     https://openapi.programming-hero.com/api/phones?search=${searchText}
     `;
-    // console.log(url);
-    fetch(url)
-        .then(res => res.json())
-        .then(res => searchResult(res.data))
+        // console.log(url);
+        fetch(url)
+            .then(res => res.json())
+            .then(res => searchResult(res.data))
+    }
 }
 
 const searchResult = (data) => {
     const searchResult = document.getElementById('search-result');
-    data.forEach(singleData => {
+    // error-- no phone found
+    if (data.length == 0) {
+        error.innerText = 'No Phone Found';
+    }
+    searchResult.innerHTML = '';
+    const Data = data.slice(0, 20);  /*  results can not show more than 20*/
+    Data.forEach(singleData => {
         // console.log(singleData);
         const div = document.createElement('div');
         div.classList.add('col');
@@ -28,12 +41,10 @@ const searchResult = (data) => {
          <div class="card-body">
              <h5 class="card-title">${singleData.phone_name} </h5>
              <h6 class="card-title">${singleData.brand} </h6>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
-                additional content. This card has even longer content than the first to show that equal
-                height action.</p>
+                
          </div>
-        <div class="card-footer">
-            <small class="text-muted">Last updated 3 mins ago</small>
+        <div class="card-footer text-center">
+        <button type="button" class="btn btn-outline-info">Details</button>
         </div>
     </div>        
         `;
